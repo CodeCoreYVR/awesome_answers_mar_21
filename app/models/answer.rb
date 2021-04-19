c8afc3302bbdbe62791155e95bf9adcd15c86692
Naveed Raza <naveedraza@gmail.com>
2021-04-19 11:55:01 -0700

    validations

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,5 +1,6 @@
 class Answer < ApplicationRecord
   belongs_to :question
   #   validates :body, presence: true
-    validates :body, presence:{message: 'Please enter answer body'}
+    validates :body, presence:{message: 'Please enter answer body'},length: {minimum: 5 }
+    # validates :body, presence:{message: 'Please enter answer body'},length: {minimum: 10, maximum: 50}
 end
