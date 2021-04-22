fa3d14dbb251b87e6927f301cfbc92b71f38df27
Naveed Raza <naveedraza@gmail.com>
2021-04-22 12:30:02 -0700

    validation using factory_bot

---
+++ spec/factories/job_posts.rb
@@ -0,0 +1,19 @@
+#  Docuementation
+#https://github.com/thoughtbot/factory_bot/blob/master/GETTING_STARTED.md
+# 
+RANDOM_HUNDRED_CHARS='Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world'
+
+FactoryBot.define do
+  factory :job_post do
+    sequence(:title){|n| Faker::Job.title + "#{n}"}
+    # sequence is a method provede by factory_bot which acceps a lambda injecting a varaible n. n is usually a numner the factory bot increments on every object that it generates to make all instances unique
+    description {Faker::Job.field + "#{RANDOM_HUNDRED_CHARS}"}
+    location{Faker::Address.city}
+    company_name{Faker::Company.name}
+    min_salary{rand(80_000..200_000)}
+    max_salary{rand(200_000..400_000)}
+  end
+end
+
+FactoryBot.create(:job_post)
+FactoryBot.build(:job_post)
--- spec/models/job_post_spec.rb
+++ spec/models/job_post_spec.rb
@@ -1,47 +1,69 @@
 require 'rails_helper'
+RANDOM_HUNDRED_CHARS='Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world'
 
 RSpec.describe JobPost, type: :model do
   describe 'validates' do
       # Given
       # When
       # Then
-    it 'requires a title' do
-      # Given
-      job_post=JobPost.new
-      # When
-      job_post.valid?
-      # Then
-      # expect is a method and the is passed to it that we are asserting
-      # we can chain .to()
-      # .to() accepts a assertion / expectation clause
-      expect(job_post.errors.messages).to(have_key(:title))
-    end
-    it 'title is unique' do
-      # Given
-      persisted_job_post=JobPost.create(title:'full stack dev', description: "Hello world" , min_salary: 35_000)
-      job_post=JobPost.new(title: persisted_job_post.title, description: "hello world", min_salary: 35_000 )
-      # When
-      job_post.valid?
-      # Then
-      expect(job_post.errors.messages).to(have_key(:title))
-
-    end
-    it "requires a description" do
-      # Given
-      job_post=JobPost.new
-      # When
-      job_post.valid?
-      # Then
-      expect(job_post.errors.messages).to(have_key(:description))
-
-    end
-    it 'salary_min must be a number greated than 30_000' do
-      # Given
-      job_post=JobPost.new(min_salary: 25_000)
-      # When
-      job_post.valid?
-      # Then
-      expect(job_post.errors.detail[:min_salary][0][:error]).to(be(:greater_than_or_equal_to))
-    end
+      describe 'title' do
+        it 'requires a title' do
+          # Given
+          job_post=FactoryBot.build(:job_post,title: nil)
+          # When
+          job_post.valid?
+          # Then
+          # expect is a method and the is passed to it that we are asserting
+          # we can chain .to()
+          # .to() accepts a assertion / expectation clause
+          expect(job_post.errors.messages).to(have_key(:title))
+        end
+        it 'title is unique' do
+          # Given
+          persisted_job_post=FactoryBot.create(:job_post)
+          job_post=FactoryBot.build(:job_post, title: persisted_job_post.title)
+          # When
+          job_post.valid?
+          # Then
+          expect(job_post.errors.messages).to(have_key(:title))
+        end
+      end
+      describe 'description' do
+        it "requires a description" do
+          # Given
+          job_post=FactoryBot.build(:job_post,description: nil)
+          # When
+          job_post.valid?
+          # Then
+          expect(job_post.errors.messages).to(have_key(:description))
+        end
+        it 'must be larger than 100 characters' do
+          # given
+          job_post= FactoryBot.build(:job_post,description: 'less than 100')
+          # when
+          job_post.valid?
+          # then
+          expect(job_post.errors.messages).to(have_key(:description))
+        end
+      end
+      describe 'location' do
+        it "is required " do
+          job_post=FactoryBot.build(:job_post,location: nil)
+          job_post.valid?
+          expect(job_post.errors.messages).to(have_key(:location))
+        end
+      end
+      describe 'min_salary' do
+        it 'salary_min must be a number greater than 30_000' do
+          # Given
+          job_post=FactoryBot.build(:job_post,min_salary: 25_000)
+          # When
+          job_post.valid?
+          # Then
+          expect(job_post.errors.details[:min_salary][0][:error]).to(be(:greater_than_or_equal_to))
+    # [7] pry(main)> job_post.errors.details
+    # => {:min_salary=>[{:error=>:greater_than_or_equal_to, :value=>25000, :count=>30000}]}
+        end
+      end
   end
 end
