1f73702d6c162834ae8f3f1a4d149218db9657f9
Naveed Raza <naveedraza@gmail.com>
2021-04-27 10:17:50 -0700

    Updated test for sign in user: update and new action

--- spec/controllers/job_posts_controller_spec.rb
+++ spec/controllers/job_posts_controller_spec.rb
@@ -12,25 +12,30 @@ RSpec.describe JobPostsController, type: :controller do
     # show 
 
     describe "#new" do
-        it "renders the new template " do
-            #Given
-            # User requesting to show the form / There is nothing in given
-            # When
-            get(:new) # 👈🏻 we have this get method from rails-controller-testing gem, which made magically by this get mthod else we have to create manually
-            # https://rspec.info/documentation/4.0/rspec-rails/RSpec/Rails/Matchers/RoutingMatchers/RouteHelpers.html#get-instance_method
-            # Then
-            expect(response).to(render_template(:new))# 👈🏻 We verify that the seponse will render out the template called "new" using the matcher 'render_template'
-            # response is an object that represents the HTTP-Response
-            # Rspec makes this available within the specs
-        end
-        it "sets an instance variable with a new job post" do
-            #given
-            # when
-            get(:new)
-            # then
-            # assigns(:job_post) : we have this 'assign' method available form the 'rails-controller-testing' gen that allows you to grab an instance varaible, it takes symbol(:job_posts)
-            expect(assigns(:job_post)).to(be_a_new(JobPost))
-            # we check that the instance variable @job_post is a new instance of the class JobPost(Model) - the madel that we created in the last class 
+        context "with user signed in" do
+            before do
+                session[:user_id]=FactoryBot.create(:user).id
+            end
+            it "renders the new template " do
+                #Given
+                # User requesting to show the form / There is nothing in given
+                # When
+                get(:new) # 👈🏻 we have this get method from rails-controller-testing gem, which made magically by this get mthod else we have to create manually
+                # https://rspec.info/documentation/4.0/rspec-rails/RSpec/Rails/Matchers/RoutingMatchers/RouteHelpers.html#get-instance_method
+                # Then
+                expect(response).to(render_template(:new))# 👈🏻 We verify that the seponse will render out the template called "new" using the matcher 'render_template'
+                # response is an object that represents the HTTP-Response
+                # Rspec makes this available within the specs
+            end
+            it "sets an instance variable with a new job post" do
+                #given
+                # when
+                get(:new)
+                # then
+                # assigns(:job_post) : we have this 'assign' method available form the 'rails-controller-testing' gen that allows you to grab an instance varaible, it takes symbol(:job_posts)
+                expect(assigns(:job_post)).to(be_a_new(JobPost))
+                # we check that the instance variable @job_post is a new instance of the class JobPost(Model) - the madel that we created in the last class 
+            end
         end
     end
     describe '#create' do
@@ -176,26 +181,31 @@ RSpec.describe JobPostsController, type: :controller do
             #given
             @job_post=FactoryBot.create(:job_post)
         end
-        context "with valid parameters" do
-            it "update the job post record with the new attributes" do
-                #when
-                new_title="#{@job_post.title} plus some changes!!!"
-                patch :update, params:{id: @job_post.id, job_post: {title: new_title}}
-                #then
-                expect(@job_post.reload.title).to eq new_title
+        context "with signed in user" do
+            before do
+                session[:user_id]=FactoryBot.create(:user)
+            end 
+            context "with valid parameters" do
+                it "update the job post record with the new attributes" do
+                    #when
+                    new_title="#{@job_post.title} plus some changes!!!"
+                    patch :update, params:{id: @job_post.id, job_post: {title: new_title}}
+                    #then
+                    expect(@job_post.reload.title).to eq new_title
+                end
+                it "redirects to the show page" do
+                    new_title="#{@job_post.title} plus some changes!!!"
+                    patch :update, params:{id: @job_post.id, job_post:{title: new_title}}
+                    expect(response).to redirect_to(@job_post)
+                end
             end
-            it "redirects to the show page" do
-                new_title="#{@job_post.title} plus some changes!!!"
-                patch :update, params:{id: @job_post.id, job_post:{title: new_title}}
-                expect(response).to redirect_to(@job_post)
-            end
-        end
-        context "with invalid parameters" do
-            it "should not update the job_post record in the db" do
-                patch :update, params: {id: @job_post.id, job_post: {title: nil}}
-                # we will grab the valid job_post and make it invalid
-                job_post_after_update = JobPost.find(@job_post.id)
-                expect(job_post_after_update.title).to eq @job_post.title
+            context "with invalid parameters" do
+                it "should not update the job_post record in the db" do
+                    patch :update, params: {id: @job_post.id, job_post: {title: nil}}
+                    # we will grab the valid job_post and make it invalid
+                    job_post_after_update = JobPost.find(@job_post.id)
+                    expect(job_post_after_update.title).to eq @job_post.title
+                end
             end
         end
         
