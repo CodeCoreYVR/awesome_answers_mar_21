10286f7b9d8557baa341f49e503bd1f932c1863e
Naveed Raza <naveedraza@gmail.com>
2021-05-03 10:01:27 -0700

    working on new_answer email

--- app/mailers/answer_mailer.rb
+++ app/mailers/answer_mailer.rb
@@ -3,7 +3,8 @@
 # https://guides.rubyonrails.org/action_mailer_basics.html
 
 class AnswerMailer < ApplicationMailer
-
+    # In a mailer class the public methods are user to create and send mails.
+    # they are simillar to actions in a controller
     def hello_world
         mail(
             to: 'naveed@codecore.ca',
@@ -11,6 +12,16 @@ class AnswerMailer < ApplicationMailer
             cc: 'jj@movies.com',
             bcc: 'someone.else@example.com',
             subject: 'Hello World!'
+        )
+    end
+    def new_answer(answer)
+        # Any instance variable set in mailer will be available in its rendered templates
+        @answer=answer
+        @question=answer.question
+        @question_owner=@question.user
+        mail(
+            to: @question_owner.email,
+            subject: "#{answer.user.first_name} answered your question!"
         )
     end
     
